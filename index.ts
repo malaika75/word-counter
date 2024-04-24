bedb15751a57a1b55079f0b312a8514ce8864db6
malaika75 <malaika57680@gmail.com>
2024-04-24 18:56:57 +0500

    word and letter counter project

---
+++ index.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/enr node
+
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt ([
+   
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count words:",
+    }
+
+])
+const words = answer.sentence.trim().split(" ")
+console.log (words)
+const letters = answer.sentence.trim().replace(/\s+/g, "")
+console.log (letters)
+console.log (`Number of letters: ${letters.length}`)
+console.log(`Number of  words :  ${words.length}`)
